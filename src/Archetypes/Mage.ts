f6ca9d2b38c2b50c11faabf5dd2950d91421ec59
andre-usf <andrefretta@gmail.com>
2023-03-24 18:11:28 -0300

    fix: extensões das classes archetypes.

--- src/Archetypes/Mage.ts
+++ src/Archetypes/Mage.ts
@@ -1,24 +1,18 @@
 import Archetype from './Archetype';
-import Energy, { EnergyType } from '../Energy';
+import { EnergyType } from '../Energy';
 
-export default class Mage extends Archetype implements Energy {
+export default class Mage extends Archetype {
   private _type: EnergyType;
-  private _amount: number;
   private static _archetypeInstances = 0;
 
   constructor(name: string) {
     super(name);
     this._type = 'mana';
-    this._amount = 0;
     Mage._archetypeInstances += 1;
   }
 
   get energyType(): EnergyType {
     return this.type_;
-  }
-
-  get amount(): number {
-    return this._amount;
   }
 
   get type_(): EnergyType {
--- src/Archetypes/Warrior.ts
+++ src/Archetypes/Warrior.ts
@@ -1,24 +1,18 @@
 import Archetype from './Archetype';
-import Energy, { EnergyType } from '../Energy';
+import { EnergyType } from '../Energy';
 
-export default class Warrior extends Archetype implements Energy {
+export default class Warrior extends Archetype {
   private _type: EnergyType;
-  private _amount: number;
   private static _archetypeInstances = 0;
 
   constructor(name: string) {
     super(name);
     this._type = 'stamina';
-    this._amount = 0;
     Warrior._archetypeInstances += 1;
   }
 
   get energyType(): EnergyType {
     return this.type_;
-  }
-
-  get amount(): number {
-    return this._amount;
   }
 
   get type_(): EnergyType {
